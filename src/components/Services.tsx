e259995bf7f0a75cde335c33e5bba84d44967c75
Vaskovic <adam.vaskovic@sogeti.com>
2025-02-04 15:43:12 +0100

    Changed vtconfig

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -39,7 +39,7 @@ const Services = () => {
             Our Services
           </span>
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3xl:grid-cols-5 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-8">
           {services.map((service, index) => (
             <Card 
               key={index} 
